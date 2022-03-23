ccd01fbd935532a54bdc3729a42b6ce5792652dd
BGMP <jose@bgmp.cl>
2022-03-23 11:54:52 -0300

    Add a distribute script

---
+++ distribute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+readonly DIST=/home/deploy/distribute
+
+bundle exec rake
+
+[ -e $DIST/rva_data/yml ] && rm -r $DIST/rva_data/yml
+[ -e $DIST/rva_data/yaml ] && rm -r $DIST/rva_data/yaml
+[ -e $DIST/rva_data/rva_data.json ] && rm $DIST/rva_data/rva_data.json
+
+mv yml $DIST/rva_data
+mv yaml $DIST/rva_data
+mv rva_data.json $DIST/rva_data
