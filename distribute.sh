97cd5dbc8fec4b16741b1eea6fceefeba256af86
BGMP <jose@bgmp.cl>
2022-03-23 12:31:05 -0300

    Ensure rva_data exists before deploying

--- distribute.sh
+++ distribute.sh
@@ -4,6 +4,7 @@ readonly DIST=/home/deploy/distribute
 
 bundle exec rake
 
+mkdir -p $DIST/rva_data
 [ -e $DIST/rva_data/yml ] && rm -r $DIST/rva_data/yml
 [ -e $DIST/rva_data/yaml ] && rm -r $DIST/rva_data/yaml
 [ -e $DIST/rva_data/rva_data.json ] && rm $DIST/rva_data/rva_data.json
