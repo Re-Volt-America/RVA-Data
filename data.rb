7303ae43fd4bc3233ad5efc624b41af684b1ee5e
BGMP <jose@bgmp.cl>
2022-06-21 01:59:33 -0400

    Update to version 22.0621a-1

--- data.rb
+++ data.rb
@@ -2,9 +2,9 @@
 
 module RVAData
   YEAR = 22
-  MONTH = 5
-  DAY = 3
-  REVISION = 3
+  MONTH = 6
+  DAY = 21
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
 end
