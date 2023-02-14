02fcf166f3166fc6ee84667736b09d3762e068ea
José Benavente <jose@bgm.cl>
2023-02-14 02:38:41 -0300

    Bump version

--- data.rb
+++ data.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module RVAData
-  YEAR = 22
-  MONTH = 12
-  DAY = 12
-  REVISION = 5
+  YEAR = 23
+  MONTH = 2
+  DAY = 14
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
 end
