b2543375b9e15cd8715bbef17aad0f9810a02f83
José Benavente <jose@bgm.cl>
2023-05-22 00:44:22 -0400

    Bump revision

--- data.rb
+++ data.rb
@@ -4,7 +4,7 @@ module RVAData
   YEAR = 23
   MONTH = 5
   DAY = 16
-  REVISION = 1
+  REVISION = 2
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
 end
