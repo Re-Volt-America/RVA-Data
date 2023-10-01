08e4722ccd7c40738293287683b09b57c3da2c0e
José Benavente <jose@bgm.cl>
2023-10-01 12:33:42 -0300

    Fix version format

--- data.rb
+++ data.rb
@@ -6,5 +6,5 @@ module RVAData
   DAY = 16
   REVISION = 3
   SUFFIX = 'a'
-  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
+  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 end
