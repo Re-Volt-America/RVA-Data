21d0216c390fe30d283953c70df5e9c50e1c06b2
José Benavente <jose@bgm.cl>
2023-10-01 12:34:27 -0300

    Update to version 23.1001a-1

--- data.rb
+++ data.rb
@@ -2,9 +2,9 @@
 
 module RVAData
   YEAR = 23
-  MONTH = 5
-  DAY = 16
-  REVISION = 3
+  MONTH = 10
+  DAY = 1
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 end
