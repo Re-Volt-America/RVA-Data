1d10d25b1e4c8bced24c4d8cda2191e7dd79def2
José Benavente <jose@bgm.cl>
2024-05-01 00:47:10 -0400

    Update to version 24.0501a-1

--- data.rb
+++ data.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module RVAData
-  YEAR = 23
-  MONTH = 10
-  DAY = 30
-  REVISION = 4
+  YEAR = 24
+  MONTH = 5
+  DAY = 1
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 end
