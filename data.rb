c2b713d8ed178e985a1969f3af8bb9c4a095810e
EstebanMz <esja.mz10@gmail.com>
2025-01-05 16:07:07 -0500

    Bump revision

--- data.rb
+++ data.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module RVAData
-  YEAR = 24
-  MONTH = 5
-  DAY = 1
-  REVISION = 18
+  YEAR = 25
+  MONTH = 1
+  DAY = 5
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 end
