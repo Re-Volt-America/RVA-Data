6f2903dac27f0a9adfe31e95bad1731e4ae56a26
EstebanMz <esja.mz10@gmail.com>
2025-08-25 12:50:02 -0500

    Bump revision
    New season 2025-2 starts here

--- data.rb
+++ data.rb
@@ -2,9 +2,9 @@
 
 module RVAData
   YEAR = 25
-  MONTH = 1
-  DAY = 5
-  REVISION = 11
+  MONTH = 8
+  DAY = 25
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 end
